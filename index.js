bca91c2bafd18847fe1d8a06b6b5040ab024bd9f
felipetalmeida <felipetalmeida@gmail.com>
2024-11-21 20:39:34 -0300

    desafio classificador de Nível de Heroi

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("oi")
+let nomeHeroi = "Chandanga"
+let experiencia = 2500
+let nivel = ""
+
+if(experiencia >= 10001){
+    nivel = "Radiante"
+}
+else if(experiencia >= 9001 && experiencia <= 10000){
+    nivel = "Imortal"
+}
+else if(experiencia >= 8001 && experiencia <= 9000){
+    nivel = "Ascendente"
+}
+else if(experiencia >= 7001 && experiencia <= 8000){
+    nivel = "Platina"
+}
+else if(experiencia >= 5001 && experiencia<= 7000){
+    nivel = "Ouro"
+}
+else if(experiencia >= 2001 && experiencia <= 5000){
+    nivel = "Prata"
+}
+else if(experiencia >= 1001 && experiencia <= 2000){
+    nivel = "Imortal"
+}
+else if(experiencia <= 1000){
+    nivel = "Ferro"
+}
+
+console.log(nivel)
